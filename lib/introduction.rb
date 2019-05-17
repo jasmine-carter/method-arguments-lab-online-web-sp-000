39b561b8d44d8cc6736542d03257e3b0bed01a72
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-17 01:38:31 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,5 +3,5 @@ puts "Hi, my name is #{name}.\n"
 end
 
 def introduction_with_language(name, language)
-"Hi, my name is #{name} and I am learning to program in #{language}.\n"
+puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
